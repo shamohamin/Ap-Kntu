f1cce83fb8eb128ba38a9de1bc9c8b3ab28ebf0e
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-03-28 20:41:40 +0430

    added width query

--- components/HomeComponent/Home.js
+++ components/HomeComponent/Home.js
@@ -1,4 +1,4 @@
-import React,{useEffect , useState} from 'react';
+import React,{useEffect , useState, useLayoutEffect} from 'react';
 import "../../style/Home.css";
 import { useRouter } from "next/router";
 import { HomeToolbar } from './HomeToolbar';
@@ -101,8 +101,29 @@ const Home = () => {
         }
     }
 
+    const makeWidthBetter = () => {
+        $('.project-menu').toggle(10 ,'linear' ,() => {
+            if(!$('.project-menu span').is(':visible')){
+                $(".home-component .main-row div[class^='col-8']").not('.console').removeClass().addClass('col-10');
+                $(".home-component .main-row div[class^='col-4' ]").not('.console').removeClass().addClass('col-1');
+                $('.home-component .toggle-project-menu').css({'border':'none', 'cursor':'pointer' , 'transform':'rotate(90deg)', 'padding-down':'40px', 'padding-left' : '30px'});
+                $('.toggle-project-menu span').hide();
+            }else{
+                $(".home-component .main-row div[class^='col-10']").not('.console').removeClass().addClass('col-8').css({'border-left' : 'none'});
+                $(".home-component .main-row div[class^='col-1']").not('.console').removeClass().addClass('col-4');
+                $('.home-component .toggle-project-menu').attr('style' , 'border-bottom: 1px solid white; cursor:pointer ');
+                $('.toggle-project-menu span').show();
+            }
+            $('.toggle-project-menu span')
+            .removeClass(`${$('.project-menu span').is(':visible') ? 'fa-angle-right' : 'fa-angle-down' }`)
+            .addClass(`${$('.project-menu span').is(':visible') ? 'fa-angle-down' : 'fa-angle-right' }`)
+        })
+    }
 
     useEffect(() => {
+        if(window.innerWidth < '600'){
+            makeWidthBetter();
+        }
         updateWord();
     }, []);
 
@@ -128,7 +149,6 @@ const Home = () => {
         }
     }
 
-
     return <React.Fragment>
         <div className="home-component">
             <div className="container-made">
@@ -153,33 +173,37 @@ const Home = () => {
                 </div>
                     <div style={{marginLeft:'-10px',color:'white'}}>
                         <div className="container">
-                            <div className="row">
-                                <div className="col-4"style={{color:'white'}} >
-                                    <div style={{borderBottom: '1px solid white'}}>Project<span style={{paddingLeft:'5px' , paddingTop:'5px'}} className="fas fa-angle-down"></span></div>
+                            <div className="row main-row">
+                                <div  className="col-4" style={{color:'white'}} >
+                                    <div onClick={() => makeWidthBetter() }
+                                        className="toggle-project-menu" style={{borderBottom:'1px solid white', cursor:'pointer'}}>Project<span style={{paddingLeft:'5px' , paddingTop:'5px'}} className="fas fa-angle-down"></span></div>
                                     <div className="project-menu">
-                                        <div> <span className="fas fa-caret-down"></span> Classroom</div>
+                                        <div  className="class-room"> <span style={{ cursor:'pointer'}} onClick={() => {$('.project-menu * ').not('.class-room').toggle(10 , 'linear' , () => 
+                                                        $('#class-room-span')
+                                                        .removeClass(`${$('.project-menu span').is(':visible') ? 'fa-caret-right' : 'fa-caret-down' }`)
+                                                        .addClass(`${$('.project-menu span').is(':visible') ? 'fa-caret-down' : 'fa-caret-right' }`))}} id="class-room-span" className="class-room fas fa-caret-down"></span> Classroom</div>
                                         <div className="ml-2"> <span className="fas fa-caret-right"> </span> .idea</div>
                                         <div className="ml-2"> 
                                             <span className="fas fa-caret-down src">  src </span>
-                                            <div className="ml-2"> 
-                                                <span onClick={() => onToggle('main')} className="fas fa-caret-down main">  main </span> 
+                                            <div className="ml-2">
+                                                <span data-toggle="data-toggle" onClick={() => onToggle('main')} className="fas fa-caret-down main">  main </span> 
                                                 <div className="ml-2"> 
-                                                    <span onClick={() => onToggle('java')} className="fas fa-caret-down java"> java  </span> 
+                                                    <span data-toggle="data-toggle" onClick={() => onToggle('java')} className="fas fa-caret-down java"> java  </span> 
                                                     <div className="ml-2">
-                                                        <span onClick={() => onToggle('ir-ac-kntu')} className="fas fa-caret-down ir-ac-kntu">  ir.ac.kntu </span>
+                                                        <span data-toggle="data-toggle" onClick={() => onToggle('ir-ac-kntu')} className="fas fa-caret-down ir-ac-kntu">  ir.ac.kntu </span>
                                                         <div style={{cursor:'pointer'}} onClick={() => {
                                                             showPMD(false);
                                                             showCheckStyle(false);
-                                                        }} className="ml-2"> <span  style={{color:'red'}} className="fab fa-java"><span> Classroom.java</span> </span>  </div>
+                                                        }} className="ml-2"> <span  style={{color:'red'}} className="fab fa-java"><span style={{color:'white'}}> Classroom.java</span> </span>  </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="ml-2">
-                                                <span onClick={() => onToggle('test')} data-toggle="toggle" className="fas fa-caret-down test">  test </span>
+                                            <div className="ml-2 class-toggle">
+                                                <span data-toggle="data-toggle" onClick={() => onToggle('test')} className="fas fa-caret-down test">  test </span>
                                                     <div className="ml-2">
-                                                        <span onClick={() => onToggle('java-test')} data-toggle="toggle" className="fas fa-caret-down java-test"> java </span>
+                                                        <span data-toggle="data-toggle" onClick={() => onToggle('java-test')} className="fas fa-caret-down java-test"> java </span>
                                                             <div className="ml-2">
-                                                                <span onClick={() => onToggle('ir-ac-kntu-style')} data-toggle="toggle" className="fas fa-caret-down ir-ac-kntu-style"> ir.ac.kntu.style </span>
+                                                                <span data-toggle="data-toggle" onClick={() => onToggle('ir-ac-kntu-style')} className="fas fa-caret-down ir-ac-kntu-style"> ir.ac.kntu.style </span>
                                                                 <div className="ml-2">
                                                                     <span style={{color:'red' , display:"block", marginBottom:'3px'}} className="fab fa-java"><span style={{color:'white',fontSize:'17px'}}> SolutionTest.java </span></span> 
                                                                     <span style={{color:'red' , display:"block", marginBottom:'3px' , cursor:'pointer'}} className="fab fa-java"><span onClick={() => {   
@@ -193,11 +217,8 @@ const Home = () => {
                                                                     <span style={{display:"block", marginBottom:'3px'}} className="fas fa-file-code"><span style={{color:'white',fontSize:'17px'}}> config.xml </span></span> 
                                                                     <span style={{display:"block", marginBottom:'3px'}} className="fas fa-file-code"><span style={{color:'white',fontSize:'17px'}}> naming.xml </span></span> 
                                                                 </div>
-                                                                
                                                             </div>
-                                                        
                                                     </div>
-                                                
                                             </div>
                                         </div>
                                         <div className="ml-2"> <span className="fas fa-caret-right"> </span> target </div>
@@ -206,27 +227,31 @@ const Home = () => {
                                         <div className="ml-1"> <span style={{color:'gray'}} className="fas fa-terminal"> </span> Scratches and Console</div>
                                     </div>
                                 </div>
+                                
                                 <div style={{borderBottom:'1px solid gray' }} className="col-8">
                                     <div className="row" style={{borderBottom:'1px solid white', paddingTop:'5px'}}>
                                         <span style={{color:'red', borderRight:'1px solid gray' , padding:'1px' , paddingRight:'10px' ,display:"block"}} className="fab fa-java"><span style={{color:'white', fontSize:'17px'}}> { PMD ? 'CheckPMDTest.java' : 'Classroom.java' }</span></span> 
                                     </div>
-                                    <div style={{height:'400px',width:'100%',overflowY:'scroll', overflowX:'scroll'}}>
+                                    <div id="content" style={{height:'400px',width:'100%',overflowY:'scroll', overflowX:'scroll'}}>
                                         <div style={{paddingTop: '10p',}}>
                                             {chooseContent()}
                                         </div>
                                     </div>
                                 </div>
+                                
                             </div>
-                            {!terminal ? <div onClick={() => showTerminal(true)}
-                                    style={{margin:'10px' ,paddingBottom:'10px',
-                                        paddingLeft:'10px' , paddingRight:'10px' , border:'1px solid gray',
-                                    height:'30px', cursor:'pointer' }}>
-                                        terminal
-                                    </div> : 
-                                <div style={{height: '200px' , border:'1px solid gray' , marginTop:'10px'}}>
-                                    <Console showTerminal={showTerminal} />
-                                </div>
-                            }
+                            <div className="row">
+                                {!terminal ? <div onClick={() => showTerminal(true)}
+                                        style={{margin:'10px' ,paddingBottom:'10px',
+                                            paddingLeft:'10px' , paddingRight:'10px' , border:'1px solid gray',
+                                        height:'30px', cursor:'pointer' }}>
+                                            terminal
+                                        </div> : 
+                                    <div className="col-12" style={{height: '200px' , border:'1px solid gray' , marginTop:'10px'}}>
+                                        <Console showTerminal={showTerminal} />
+                                    </div>
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
